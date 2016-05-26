85d6f63755c4f21b68e716aeb33d08aa2458b2f8
yurava2005 <iuliia.maksymets@gmail.com>
2016-05-26 17:41:11 +0100

    Добавлена функция distance

--- Sandbox/src/main/java/ua/stqa/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ua/stqa/sandbox/MyFirstProgram.java
@@ -3,5 +3,16 @@ package ua.stqa.sandbox;
 public class MyFirstProgram {
   public static void main(String[] args) {
     System.out.println("Hello, world!");
+    double x1 = 3;
+    double x2 = 5;
+    double y1 = 3;
+    double y2 = 8;
+
+    System.out.println("Расстояние между двумя точками с координатами (" + x1 + "," + y1 + ") и (" + x2 + "," + y2 + ") равно " + distance(x1,y1,x2,y2));
+
+
+  }
+  public static double distance (double x1, double y1, double x2, double y2){
+    return Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
   }
 }
