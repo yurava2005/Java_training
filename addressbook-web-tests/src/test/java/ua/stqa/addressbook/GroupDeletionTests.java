2fc7c8e4d81ce4b0b676e3591eaab2e4162af2ec
yurava2005 <iuliia.maksymets@gmail.com>
2016-06-03 10:41:40 +0100

    Создан класс TestBase и тест для удаления группы

---
+++ addressbook-web-tests/src/test/java/ua/stqa/addressbook/GroupDeletionTests.java
@@ -0,0 +1,20 @@
+package ua.stqa.addressbook;
+
+
+import org.testng.annotations.Test;
+
+
+public class GroupDeletionTests extends TestBase {
+
+
+  @Test
+  public void testGroupDeletion() {
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+
+  }
+
+
+}
