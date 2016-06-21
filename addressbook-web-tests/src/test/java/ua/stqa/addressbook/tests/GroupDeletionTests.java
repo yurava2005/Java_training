3e86646d8c4bc8630215aae3a4ec2838ca2ee60b
yurava2005 <iuliia.maksymets@gmail.com>
2016-06-21 09:57:59 +0100

    Проверка и обеспечение предусловий для групп

--- addressbook-web-tests/src/test/java/ua/stqa/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ua/stqa/addressbook/tests/GroupDeletionTests.java
@@ -2,6 +2,7 @@ package ua.stqa.addressbook.tests;
 
 
 import org.testng.annotations.Test;
+import ua.stqa.addressbook.model.GroupData;
 
 
 public class GroupDeletionTests extends TestBase {
@@ -10,6 +11,9 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, "test3"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
